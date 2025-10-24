d37da5c86c791391406725b42bef35d4a043886e
bxr4k <burak_ozturk@live.com>
2025-10-24 15:44:19 +0300

    Update index.php

--- index.php
+++ index.php
@@ -69,24 +69,38 @@ $stats = $pdo->query("
         
         <!-- Hızlı İşlemler -->
         <div class="row mb-4">
-            <div class="col-md-3 mb-3">
+            <div class="col-md-4 mb-3">
                 <a href="?action=bugun_benim" class="btn btn-success btn-lg w-100 quick-action-btn">
                     💰 Bugün Benim
                 </a>
             </div>
-            <div class="col-md-3 mb-3">
+            <div class="col-md-4 mb-3">
                 <a href="add_record.php" class="btn btn-primary btn-lg w-100 quick-action-btn">
                     ➕ Yeni Kayıt Ekle
                 </a>
             </div>
-            <div class="col-md-3 mb-3">
-                <a href="tel:+905415319074" class="btn btn-warning btn-lg w-100 quick-action-btn">
-                    📞 Sipariş Ver
-                </a>
-            </div>
-            <div class="col-md-3 mb-3">
+            <div class="col-md-4 mb-3">
                 <a href="reports.php" class="btn btn-info btn-lg w-100 quick-action-btn">
                     📊 Raporlar
+                </a>
+            </div>
+        </div>
+        
+        <!-- Sipariş Butonları -->
+        <div class="row mb-4">
+            <div class="col-md-6 mb-3">
+                <a href="tel:+905415319074" class="btn btn-success btn-lg w-100 quick-action-btn">
+                    📞 Ara & Sipariş Ver
+                </a>
+            </div>
+            <div class="col-md-6 mb-3">
+                <a href="https://wa.me/905415319074?text=Merhaba.%20G%C3%B6lc%C3%BCk%20Belediyesi%20Bas%C4%B1n%20Yay%C4%B1n%20M%C3%BCd%C3%BCrl%C3%BC%C4%9F%C3%BC%20i%C3%A7in%20su%20sipari%C5%9Fi%20vermek%20istiyorum." 
+                   target="_blank"
+                   class="btn btn-whatsapp btn-lg w-100 quick-action-btn">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16" style="margin-right: 5px;">
+                        <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"/>
+                    </svg>
+                    WhatsApp Sipariş
                 </a>
             </div>
         </div>
